e97d2d7617de4ab2fb12282f63fe5ee84d6e87a9
yeshu0710 <yeshwanth8520@gmail.com>
2024-01-31 02:12:40 -0700

    Imported Datasets with independent and dependent variables

--- data_processimg.py
+++ data_processimg.py
@@ -6,7 +6,7 @@ import pandas as pd
 #importing dataset
 dataset = pd.read_csv("Data.csv")
 independent_var = dataset.iloc[:,:-1].values
-dependent_var = dataset.iloc[:,-1:].values
+dependent_var = dataset.iloc[:,-1].values
 print(independent_var)
 print(dependent_var)
 #print(type(independent_var))
