be1a497646ae624ad52dbac0e45a80c425a5292d
yeshu0710 <yeshwanth8520@gmail.com>
2024-01-31 01:56:05 -0700

    Imported Dataset

--- data_processimg.py
+++ data_processimg.py
@@ -1,6 +1,14 @@
+#importing libaries
 import numpy as npy
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#importing dataset
 dataset = pd.read_csv("Data.csv")
-print(dataset)
+independent_var = dataset.iloc[:,:-1].values
+dependent_var = dataset.iloc[:,-1:].values
+print(independent_var)
+print(dependent_var)
+#print(type(independent_var))
+#print(type(dependent_var))
+
